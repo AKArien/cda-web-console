0792ba85b3940ed4fa60110e40213fbba6ceeb7b
Hue <skeptikal.monke@gmail.com>
2025-12-19 12:30:42 +0100

    deduplicate headers code

--- src/lib/headers.tsx
+++ src/lib/headers.tsx
@@ -10,13 +10,7 @@ export function anon_headers(): Headers {
 }
 
 export function auth_headers(): Headers {
-	const headers = new Headers()
-	headers.set("Content-Type", "application/json")
-	headers.set("Access-Control-Allow-Origin", "*")
-	headers.set(
-		"Access-Control-Allow-Headers",
-		"Origin, X-Requested-With, Content-Type, Accept",
-	)
+	const headers = anon_headers()
 	headers.set(
 		"Authorization",
 		"Bearer " +
