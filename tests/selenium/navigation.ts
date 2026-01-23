329dc1fd02188f9876d2163ad14ece8e7365d029
Hue <skeptikal.monke@gmail.com>
2026-01-23 22:59:45 +0100

    more navigation tests

--- tests/selenium/navigation.ts
+++ tests/selenium/navigation.ts
@@ -13,21 +13,52 @@ export function basic_nav(browser: string){
 
         await driver.get(url)
         
+        
         expect(driver.getCurrentUrl()).toBe(url + "/login")
         // login
-        const access = driver.findElement(By.name("access"))
-        const pass = driver.findElement(By.name("pass"))
-        const req_dur = driver.findElement(By.name("req_dur"))
-        const submit = driver.findElement(By.name("submit"))
-        await access.sendKeys("user")
-        await pass.sendKeys("password")
-        await req_dur.sendKeys("30")
-        await submit.click()
+        {
+            const access = driver.findElement(By.name("access"))
+            const pass = driver.findElement(By.name("pass"))
+            const req_dur = driver.findElement(By.name("req_dur"))
+            const submit = driver.findElement(By.name("submit"))
+            await access.sendKeys("user")
+            await pass.sendKeys("password")
+            await req_dur.sendKeys("30")
+            await submit.click()
+        }
 
         // being redirected here means we got logged in
-        expect(driver.getCurrentUrl()).toBe(url + "/")
+        expect(driver.getCurrentUrl()).toBe(url)
 
+        // tour every page by clicking buttons
 
+        // info/parameters
+        {
+            const access_info = driver.findElement(By.name("access"))
+            await access_info.click()
+        }
+        expect(driver.getCurrentUrl()).toBe(url + "/access")
+        {
+            const expires = driver.findElement(By.name("expires"))
+            expect(expires.getText()).toBe("Never")
+
+            const change_pass = driver.findElement(By.name("change_pass"))
+            await change_pass.click()
+            const old_pass = driver.findElement(By.name("old_pass"))
+            const pass1 = driver.findElement(By.name("pass1"))
+            const pass2 = driver.findElement(By.name("pass2"))
+            const submit = driver.findElement(By.name("submit"))
+            await old_pass.sendKeys("password")
+            await pass1.sendKeys("newpassword")
+            await pass2.sendKeys("newpassword")
+            await submit.click()
+            // todo : tell apart failure from success
+            const dashboard = driver.findElement(By.name("dashboard"))
+        }
+        expect(driver.getCurrentUrl()).toBe(url + "/dashboard")
+        {
+            
+        }
     })
 
 }
@@ -35,6 +66,7 @@ export function basic_nav(browser: string){
 export function edge_cases(browser: string){
     test("Edge cases", async () => {
         const driver = await new Builder().forBrowser(browser).build()
-
+        
+        // ensure force change password does it’s job
     })
 }
